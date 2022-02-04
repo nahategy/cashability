9e41c8192ad077ddb371653f86191025ed770b2c
mi <mi@mi-tech.hu>
2022-02-04 16:15:51 +0100

    remove spendingProp

--- src/Views/Spendings/List.tsx
+++ src/Views/Spendings/List.tsx
@@ -1,9 +1,10 @@
 import SpendingItem from "../../Components/Spending/Spending";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../store";
 import {Spending} from "../../Types";
 import {useParams} from "react-router-dom";
 import {getCurrentYYMM} from "../../Utils";
+import {remove} from "../../GlobalState/SpendingSlice";
 
 export type ListUrlParameters = {
     date?: string
@@ -11,16 +12,20 @@ export type ListUrlParameters = {
 
 function List() {
     const spendingResponse = useSelector((state: RootState) => state.spendingResponse.spendingResponse)
+    const dispatch = useDispatch();
     let {date} = useParams<ListUrlParameters>();
     if (!date) {
         date = getCurrentYYMM();
     }
     const spendings: Spending[] = spendingResponse[date] ?? [];
+    const removeSpending = (spending: Spending): void => {
+        dispatch(remove(spending))
+    }
 
     return (
         <div>
             {spendings && spendings.length > 0 ? spendings.map((value) => (
-                <SpendingItem {...value} key={value.id}/>
+                <SpendingItem spending={value} remove={removeSpending} key={value.id}/>
             )) : `No spending for the date of ${date}`
             }
         </div>
