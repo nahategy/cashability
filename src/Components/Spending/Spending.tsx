1b25546c7800b9a2d51a9ef82f6df6a7b83a4b25
mi <mi@mi-tech.hu>
2022-02-04 16:15:15 +0100

    remove spending event

--- src/Components/Spending/Spending.tsx
+++ src/Components/Spending/Spending.tsx
@@ -1,12 +1,26 @@
 import {Spending} from "../../Types";
 import {stringToFormattedDateString} from "../../Utils";
 
-function SpendingItem(spending: Spending) {
+
+export interface SpendingItemProps {
+    spending: Spending;
+    remove: { (spending: Spending): void };
+}
+
+function SpendingItem(props: SpendingItemProps) {
+    const spending = props.spending;
+    const remove = props.remove
+
+    const removeSpending = () => {
+        remove(spending);
+    }
+
     return (
         <div>
+            <div onClick={removeSpending}>Remove</div>
             <span>Name: {spending.name}</span>
             <span>Amount: {spending.amount}</span>
-            <span>Date: {spending.date ? stringToFormattedDateString(`${spending.date}`)  : ""}</span>
+            <span>Date: {spending.date ? stringToFormattedDateString(`${spending.date}`) : ""}</span>
             <span>Synced: {spending.id ? "Y" : "N"}</span>
         </div>
     )
